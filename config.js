0206cc49fd657cbc4e40dbb97d40bfb76954bae6
Next-Level-Dev <shadowdiary01@gmail.com>
2025-02-28 19:42:37 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const correctPassword = "h8pux";
+
+const files = [
+    "Stairs That Consume.pdf",
+    "The Throath.pdf",
+    "Un-suspicious Dead Bodies.pdf"
+];
+
+const disclaimerText = "<p>This is a fictional writing project, nothing is actually true or confidential. The password is <span style='color: #00ff00;'>\"h8pux\"</span></p>";
